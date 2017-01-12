00841ff8ded4d1a0557b4d08ad3d1eafbbfd9913
Junnama Noda <junnama@alfasado.jp>
2017-01-12 16:43:23 +0900

    Only if MT::App::CMS::Smartphone.

--- mt/plugins/SmartphoneRebuild/lib/SmartphoneRebuild/Callbacks.pm
+++ mt/plugins/SmartphoneRebuild/lib/SmartphoneRebuild/Callbacks.pm
@@ -3,9 +3,10 @@ use strict;
 
 sub build_file_filter {
     my ( $cb, %args ) = @_;
+    my $mt = MT->instance();
+    return 1 if ( (ref $mt) ne 'MT::App::CMS::Smartphone' );
     return 1 if MT->request( 'init_plugins_for_smartphone_rebuild' );
     MT->request( 'init_plugins_for_smartphone_rebuild', 1 );
-    my $mt = MT->instance();
     my $cfg          = $mt->config;
     my $use_plugins  = $cfg->UsePlugins;
     my @PluginPaths;
